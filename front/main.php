a5417541392034a8c9bb87db9c7394c7c6cb7fb8
jing <jinng.vip@gmail.com>
2024-07-15 15:32:16 +0800

    新增電影預告片海報網頁,引入front/main.php

--- front/main.php
+++ front/main.php
@@ -1,14 +1,115 @@
 <div class="half" style="vertical-align:top;">
     <h1>預告片介紹</h1>
     <div class="rb tab" style="width:95%;">
-        <div id="abgne-block-20111227">
-            <ul class="lists">
-            </ul>
-            <ul class="controls">
-            </ul>
+        <div id="posters">
+            <div class="lists">
+                <?php
+                $posters = $Poster->all(['sh' => 1], " Order By rank");
+                foreach ($posters as $poster) {
+                ?>
+                    <div class='poster' data-ani='<?= $poster['ani']; ?>'>
+                        <img src="./images/<?= $poster['img']; ?>">
+                        <div class='name'><?= $poster['name']; ?></div>
+                    </div>
+                <?php
+
+                }
+                ?>
+            </div>
+            <div class="controls">
+                <div class="control">
+                    <div class="btn-left"></div>
+                </div>
+                <div class="btns">
+                    <?php
+                    foreach ($posters as $poster) {
+                    ?>
+                        <div class="btn">
+                            <img src="./images/<?= $poster['img']; ?>" alt="">
+                            <div><?= $poster['name']; ?></div>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+                <div class="control">
+                    <div class="btn-right"></div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+<script>
+    $(".poster").eq(0).show()
+    let now = 0;
+    let next = 1;
+    let slide = setInterval(() => {
+        ani()
+    }, 3000)
+
+    function ani() {
+
+        let ani = parseInt($(".poster").eq(now).data('ani'))
+
+        if (now < $(".poster").length - 1) {
+            // 使用next=now + 1，將不會更動到now的值。（勿使用now++，以免誤將now值異常變更）
+            next = now + 1;
+        } else {
+            next = 0
+        }
+        //console.log(ani, now, next)
+        switch (ani) {
+            case 1:
+                //淡入淡出
+                $(".poster").eq(now).fadeOut(1000, () => {
+                    $(".poster").eq(next).fadeIn(1000)
+                })
+                break;
+            case 2:
+                //縮放
+                $(".poster").eq(now).hide(1000, () => {
+                    $(".poster").eq(next).show(1000)
+
+                })
+                break;
+            case 3:
+                //滑入滑出
+                $(".poster").eq(now).slideUp(1000, () => {
+                    $(".poster").eq(next).slideDown(1000)
+                })
+                break;
+        }
+
+        now = next;
+        // 
+
+    }
+
+
+    $(".btn-left,.btn-right").on("click", function() {
+        console.log()
+        let direction = $(this).attr('class').split("-")[1]
+        switch (direction) {
+            case 'left':
+
+                $(".btn").animate({
+                    right: 80
+                });
+
+                break;
+            case 'right':
+
+                $(".btn").animate({
+                    right: 0
+                });
+                break;
+        }
+    })
+</script>
+
+
+
 <div class="half">
     <h1>院線片清單</h1>
     <div class="rb tab" style="width:95%;">
