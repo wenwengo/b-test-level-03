fdbd3235073b37f3bb89d15bcb423288d80fa217
jing <jinng.vip@gmail.com>
2024-08-05 14:59:14 +0800

    補完題3練習

--- api/load_seats.php
+++ api/load_seats.php
@@ -1,3 +1,4 @@
+<?php include_once "base.php"; ?>
 <style>
     .theater {
         width: 540px;
@@ -25,11 +26,11 @@
     }
 
     .null {
-        background: url('../icon/03D02.png') center no-repeat;
+        background: url('./icon/03D02.png') center no-repeat;
     }
 
     .booked {
-        background: url('../icon/03D03.png') center no-repeat;
+        background: url('./icon/03D03.png') center no-repeat;
     }
 
     .chk {
@@ -37,21 +38,72 @@
         right: 5px;
         bottom: 5px;
     }
+
+    .order-info {
+        width: 540px;
+        padding: 10px;
+        background-color: #eee;
+        margin: auto;
+    }
 </style>
 <div class="theater">
     <div class="seats">
         <?php
+        $seats = [];
+        $movieName = $Movie->find($_GET['id'])['name'];
+        $orders = $Order->all(['movie' => $movieName, 'date' => $_GET['date'], 'session' => $_GET['session']]);
+        foreach ($orders as $order) {
+            $seats = array_merge($seats, unserialize($order['seats']));
+        }
+
         for ($i = 0; $i < 20; $i++) {
-            echo "<div class='seat null'>";
+            if (in_array($i, $seats)) {
+                echo "<div class='seat booked'>";
+            } else {
+                echo "<div class='seat null'>";
+                echo "<input type='checkbox' class='chk' value='$i'>";
+            }
+
             echo "<div>";
             echo (floor($i / 5) + 1) . "排" . ($i % 5 + 1) . "號";
             echo "</div>";
-            echo "<input type='checkbox' class='chk' value='$i'>";
             echo "</div>";
         }
         ?>
     </div>
 </div>
+<div class="order-info">
 
+    <div>您選擇的電影是:<?= $Movie->find($_GET['id'])['name']; ?></div>
+    <div>您選擇的時刻是：<?= $_GET['date']; ?> <?= $_GET['session']; ?></div>
+    <div>您已經勾選<span id='tickets'>0</span>張票，最多可以購買四張票</div>
+    <div class='ct'>
+        <button onclick="$('#booking,#menuBlock').toggle();seats.length=0">上一步</button>
+        <button onclick="order()">訂購</button>
+    </div>
+</div>
 
-<button onclick="$('#booking,#menuBlock').toggle()">上一步</button>
+<script>
+    $(".chk").on("click", function() {
+        if ($(this).prop('checked')) {
+
+            if (seats.length < 4) {
+                seats.push($(this).val())
+
+            } else {
+                $(this).prop("checked", false)
+                alert("最多只能選四個座位")
+            }
+        } else {
+            seats.splice(seats.indexOf($(this).val()), 1)
+        }
+        $("#tickets").text(seats.length)
+    })
+
+    function order() {
+        info['seats'] = seats;
+        $.post("api/order.php", info, function(res) {
+            $("#booking").html(res)
+        })
+    }
+</script>
